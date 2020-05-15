db5bb645cfe18bcdb709557c913b472333b7d6f0
zjl <zhangjinlong@common.com>
2020-05-15 14:44:18 +0800

    first commit

---
+++ src/main/java/com/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * author: king long
+ * date: 2020/5/15 14:14
+ * desc: use for java11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("args = " + 123);
+    }
+}
---
+++ src/main/java/com/test/entity/User.java
@@ -0,0 +1,28 @@
+package com.test.entity;
+
+/**
+ * author: king long
+ * date: 2020/5/15 14:42
+ * desc: use for java11
+ */
+public class User {
+
+    private String username;
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
